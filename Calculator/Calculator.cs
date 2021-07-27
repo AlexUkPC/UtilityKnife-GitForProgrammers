694d006183f225dbdfd891029cf00aeb022868c6
AlexUkLenovoYoga <alcatraz1987z@yahoo.com>
2021-07-27 08:01:35 -0700

    Add Subtract method

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,5 +6,9 @@ namespace UtilityKnife.Calculator
         {
             return x + y;
         }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
     }
 }
