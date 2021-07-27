44220a5d0e76f6a6a2a72cb1d5babae35fed087e
AlexUkPC <alexrogna@yahoo.com>
2021-07-27 16:17:12 +0100

    change to double

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -2,15 +2,15 @@ namespace UtilityKnife.Calculator
 {
     public class Calculator
     {
-        public static int Add (int x, int y)
+        public static double Add (double x, double y)
         {
             return x + y;
         }
-        public static int Subtract(int x, int y)
+        public static double Subtract(double x, double y)
         {
             return x - y;
         }
-        public static int Multiply(int x, int y)
+        public static double Multiply(double x, double y)
         {
             return x * y;
         }
